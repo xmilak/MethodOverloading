79acfafcc03f8c8d8475d78d6a974541b510a474
Neven Milakovic <xxmilak@gmail.com>
2023-03-14 10:16:43 -0400

    finished work

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MethodOverloading
+{
+    public class MyClass
+    {
+        public static void Main()
+        {
+             
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isDollars)
+        {
+            int sum = a + b;
+            if (isDollars)
+            {
+                if (sum == 1)
+                {
+                    return $"{sum}dollar";
+                }
+       
+                    
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+          
+        }
+    }
+}
